0c7a4deac942cae186286ea73b7b491ef9de2646
Shenchao Guo <v-sguo@microsoft.com>
2023-10-24 11:01:54 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,12 @@ version = '0.1.0'
 
 # -- General configuration
 
+html_context = {
+    "display_github": False,
+    "last_updated": True,
+    "commit": False,
+}
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -18,14 +24,6 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
-
-html_context = {
-    "display_github": True, # 是否显示编辑按钮
-    "display_bitbucket": True, # 是否显示Bitbucket链接
-    "display_gitlab": False, # 是否显示GitLab链接
-    "display_toc": False, # 是否显示目录
-    "collapse_navigation": False, # 是否折叠导航
-}
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
