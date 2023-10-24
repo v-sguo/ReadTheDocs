7a22da75dab5468fbf8256d168a71e765a0b9979
Shenchao Guo <v-sguo@microsoft.com>
2023-10-24 17:28:04 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -28,13 +28,6 @@ html_context = {
 "commit": False,
 }
 
-extensions = [
-    'sphinx.ext.pngmath',
-    'sphinx.ext.ifconfig',
-    'sphinx_git',
-    'sphinxcontrib.email',
-]
-
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
