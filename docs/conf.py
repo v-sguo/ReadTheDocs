5003cdc6b0d7d071ce89b7354e7523779ce8a2c2
Shenchao Guo <v-sguo@microsoft.com>
2023-10-24 17:24:57 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,17 @@
 ﻿# Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import sys
+import os
+
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 project = 'ReadTheDocs'
 copyright = '2021, v-sguo'
@@ -16,6 +27,13 @@ html_context = {
 "last_updated": True,
 "commit": False,
 }
+
+extensions = [
+    'sphinx.ext.pngmath',
+    'sphinx.ext.ifconfig',
+    'sphinx_git',
+    'sphinxcontrib.email',
+]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
