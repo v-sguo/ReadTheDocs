8727a7642881dd1cfb59dbae09a6667518f918f1
Shenchao Guo <v-sguo@microsoft.com>
2023-10-25 17:27:40 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,34 +1,35 @@
-﻿# conf.py
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+﻿﻿# Configuration file for the Sphinx documentation builder.
 
-# -- Project information -----------------------------------------------------
+# -- Project information
 
-project = 'My Project'
-copyright = '2023, My Name'
-author = 'My Name'
+project = 'ReadTheDocs'
+copyright = '2021, v-sguo'
+author = 'v-sguo'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1'
+version = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static']
-
-html_context = {
-    "display_github": True,  # 不显示 'Edit on Github' 链接，而是显示 'View page source'
-    "last_updated": True,
-    "commit": False,
-}
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
