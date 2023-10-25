6f7faf2922d184335005877ac6b62dc7d4218e70
Shenchao Guo <v-sguo@microsoft.com>
2023-10-25 13:45:54 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -4,12 +4,6 @@ author = 'v-sguo'
 
 # -- General configuration
 
-html_context = {
-  "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
-  "last_updated": True,
-  "commit": False,
-}
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -35,3 +29,5 @@ epub_show_urls = 'footnote'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = []
+
+
