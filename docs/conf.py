9f4c48d914cb9e90b3632630e3b636b920b2b362
Shenchao Guo <v-sguo@microsoft.com>
2023-10-25 17:25:35 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
 
