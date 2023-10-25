efafce99190cc5973e37c3d0bd8f7a1a3a6dbf89
Shenchao Guo <v-sguo@microsoft.com>
2023-10-25 14:01:04 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,13 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+#
+html_theme_options = {
+  "display_version": False,
+  "logo_only": True,
+  "style_nav_header_background": "#151033",
+}
+
 # -- Options for HTML output
 html_theme = 'sphinx_rtd_theme'
 
