4ea06573bc8de8bc58918db0b7a547adc3300a88
Shenchao Guo <v-sguo@microsoft.com>
2023-10-25 09:17:56 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,12 @@ version = '0.1.0'
 
 # -- General configuration
 
+html_context = {
+  "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+  "last_updated": True,
+  "commit": False,
+}
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
