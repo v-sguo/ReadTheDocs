b5a5fc38c214db492f730788c857f8072ed49d23
Shenchao Guo <v-sguo@microsoft.com>
2023-10-26 10:17:10 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -29,5 +29,26 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+  "display_version": False,
+  "logo_only": True,
+  "style_nav_header_background": "#151033",
+}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#
+html_logo = "_static/amulet-logo2021.png"
+
+# The name of an image file (relative to this directory) to use as a favicon of
+# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#
+html_favicon = "_static/amlt-logo2021.ico"
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
